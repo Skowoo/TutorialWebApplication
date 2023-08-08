908a35bb93c7822467623ea8ebfc01b33983e20f
Skowoo <skowoo@tlen.pl>
2023-08-08 16:53:07 +0200

    Tutorial finished. (Step 8, data validation)

--- TutorialWebApplication/Migrations/20230808145157_New_DataAnnotations.Designer.cs
+++ TutorialWebApplication/Migrations/20230808145157_New_DataAnnotations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TutorialWebApplication.Data;
 
@@ -11,9 +12,10 @@ using TutorialWebApplication.Data;
 namespace TutorialWebApplication.Migrations
 {
     [DbContext(typeof(TutorialWebApplicationContext))]
-    partial class TutorialWebApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230808145157_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
