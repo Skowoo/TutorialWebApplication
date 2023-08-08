d25eb6cfc39f8f75564c6f9bc31a5bcc017b878d
Skowoo <skowoo@tlen.pl>
2023-08-08 13:09:07 +0200

    Part 5 done

--- TutorialWebApplication/Models/Movie.cs
+++ TutorialWebApplication/Models/Movie.cs
@@ -1,14 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TutorialWebApplication.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
         public string? Title { get; set; }
-        [DataType(DataType.Date)]
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]        
         public DateTime ReleaseDate { get; set; }
+
         public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
